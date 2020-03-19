7de8c3881210a618c73ffe3c52db9f7e2a82ef87
SlaynPool <nicolas.vadkerti@gmail.com>
2020-03-20 00:13:53 +0100

    step1

---
+++ verification.h
@@ -0,0 +1,13 @@
+#ifndef VERIFICATION_H
+#define VERIFICATION_H
+typedef struct point_t{
+    int x;
+    int y;
+}point_s;
+typedef struct courbe_t{
+    int a;
+    int b;
+    int mod;
+}courbe_s;
+
+#endif
