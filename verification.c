ac1937ed96a6d8073118092a7ef24c692b2291b3
SlaynPool <nicolas.vadkerti@gmail.com>
2020-03-20 01:39:36 +0100

    Bon les cas triviaux cest mignons mais...

--- verification.c
+++ verification.c
@@ -6,29 +6,28 @@
 // Pour debuter, on veux juste faire des verifications si des points sont sur la courbe 
 
 
-int main(int argc, char *argv[]){
-   courbe_s maCourbe;
+int verif(courbe_s *maCourbe, point_s *monPoint){
+  /* courbe_s maCourbe;
    point_s monPoint;
    maCourbe.a = atoi(argv[1]);
    maCourbe.b = atoi(argv[2]);
    maCourbe.mod = atoi(argv[3]);
    monPoint.x = atoi( argv[4]);
    monPoint.y = atoi(argv[5]);
-   printf("a= %d,b=%d,mod =%d \n",maCourbe.a,maCourbe.b, maCourbe.mod); 
-   int yy = (monPoint.y* monPoint.y)%maCourbe.mod;
-   int courbe=((monPoint.x*monPoint.x*monPoint.x)+(maCourbe.a*monPoint.x)+maCourbe.b)%maCourbe.mod;
-   printf(" yy = %d\n", yy);
-   printf(" courbe = %d\n",courbe); 
+   */
+   //printf("a= %d,b=%d,mod =%d \n",maCourbe->a,maCourbe->b, maCourbe->mod); 
+   int yy = (monPoint->y* monPoint->y)%maCourbe->mod;
+   int  courbe=((monPoint->x*monPoint->x*monPoint->x)+(maCourbe->a*monPoint->x)+maCourbe->b)%maCourbe->mod;
+   //printf(" yy = %d\n", yy);
+   //²printf(" courbe = %d\n",courbe); 
    if (yy == courbe){
-       printf("P(%d;%d) est bien sur la courbe\n",monPoint.x,monPoint.y);
+     // printf("P(%d;%d) est bien sur la courbe\n",monPoint->x,monPoint->y);
+       return 0;
    }
    else{
         
-       printf("P(%d;%d) n'est pas sur la courbe\n",monPoint.x,monPoint.y);
-   
+     // printf("P(%d;%d) n'est pas sur la courbe\n",monPoint->x,monPoint->y);
+       return 1;
    }
 
-
-
-   return 0;
 }
--- verification/verification.h
+++ verification/verification.h
@@ -2,13 +2,12 @@
 #define VERIFICATION_H
 typedef struct point_t{
     int x;
-    int  y;
+    int y;
 }point_s;
 typedef struct courbe_t{
     int a;
-    int  b;
-    int  mod;
+    int b;
+    int mod;
 }courbe_s;
 
-int verif(courbe_s *maCourbe, point_s *monPoint);
 #endif
