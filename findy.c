1ba978986208fc25327d26be5b03599d96830158
SlaynPool <nicolas.vadkerti@gmail.com>
2020-03-28 02:51:06 +0100

    delete useless comment

--- findy.c
+++ findy.c
@@ -7,30 +7,7 @@ int main(int argc, char *argv[]){
    courbe_s maCourbe;
    maCourbe.compute= 0;
    point_s monPoint;
-  /* maCourbe.a = atoi(argv[1]);
-   maCourbe.b = atoi(argv[2]);
-   maCourbe.mod = atoi(argv[3]);
-   monPoint.x = atoi( argv[4]);
-  */
-   /*
-   maCourbe.a = -3;
-   maCourbe.b = 0x64210519e59c80e70fa7e9ab72243049feb8deecc146b9b1;
-   maCourbe.mod = 6277101735386680763835789423207666416083908700390324961279;
-   monPoint.x = 4186469355574186740522149727049440186996809918465310609521;
-   Le PROBLEME est que meme dans un unsigned long long les nombres ne rentres pas
-   Par ce que après le nombre d'operations max est egale à maCourbe.mod si on fait toute les operations il ya sans doute moyen de reduire le cout
-   */
-   /*
-   maCourbe.a = 31;
-   maCourbe.b = 17;
-   maCourbe.mod = 97;
-   monPoint.x = 63;
-   */
-   /*char a;
-   char b; 
-   char mod;
-   char x;
-   */
+
    mpz_init(maCourbe.a);
    mpz_init(maCourbe.b);
    mpz_init(maCourbe.mod);
@@ -47,34 +24,15 @@ int main(int argc, char *argv[]){
    char b[]="17";
    char mod[]="97";
    char x[]="63";*/
- /*  char y[]="37";
-   mpz_set_str(monPoint.y,y,10);*/
+
    //x=63,a=31,b=17 et p=97
    mpz_set_str(maCourbe.a,a, 10);
    mpz_set_str(maCourbe.b,b, 10);
    mpz_set_str(maCourbe.mod,mod, 10);
    mpz_set_str(monPoint.x,x, 10);
-  // printf ("%d",verif(&maCourbe, &monPoint));
-       
-  //mpz_set_ui(maCourbe.b,3);
+ 
 
-
-  // int  y;
-   /*int test;
-   test= mpz_cmp(maCourbe.mod,y);
-    char * cmod;
-            //mpz_get_str(cy,10,y);
-    printf("%s\n",mpz_get_str(cmod,10,maCourbe.mod));
-    char *cy;
-            //mpz_get_str(cy,10,y);
-    printf("%s\n",mpz_get_str(cy,10,y));
-
-
-
-   printf("%d",test);*/
-
-
-/* En faite on n'a juste à resoudre le polynome courbe-y^2 = 0 
+/* En faite on n'a juste à resoudre le polynome y^2-courbe = 0 
  * si  b² – 4ac  < 0 pas de solution;
  * si b² - 4ac = 0, une solution −b/2a
  * si delta > 0 x1=−b−sqrt(Δ)/(2a) et x2=−b+sqrt(Δ)/2a
